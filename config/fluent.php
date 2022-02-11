95916a40383c164f829d80de2c1206a2b8324a8e
Chris Vasey <chrismvasey@gmail.com>
2022-02-11 12:20:45 +0000

    Set up config file to specify default and supported languages

---
+++ config/fluent.php
@@ -0,0 +1,17 @@
+<?php
+return [
+
+    // All the languages supported
+    'supported' => [
+        'en_US' => 'English',
+        'es_ES' => 'Spanish',
+        'Fr_FR' => 'French',
+        'De_DE' => 'German',
+    ],
+
+    // Our default language for saving
+    'default' => 'en_US',
+
+    //Our fluent storage path
+    'path' => 'fluent'
+];
