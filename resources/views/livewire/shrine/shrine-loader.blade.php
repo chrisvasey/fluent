dbf1f94a60d887158f76674566ca8ee93d0b7c83
Chris Vasey <chrismvasey@gmail.com>
2022-03-12 14:17:55 +0000

    Fixed layout of edit bar

--- resources/views/livewire/shrine/shrine-loader.blade.php
+++ resources/views/livewire/shrine/shrine-loader.blade.php
@@ -1,12 +1,8 @@
-@if($edit)
-    <livewire:shrine.edit-loader>
-        @livewire("shrine.pages.${slug}", [
-            'slug' => $slug
-        ])
-    </livewire:shrine.edit-loader>
-@else
-    @livewire("shrine.pages.${slug}", [
-        'slug' => $slug
-    ])
-@endif
-
+<main class="flex flex-row">
+    <div class="flex-grow">
+        @livewire("shrine.pages.${slug}", ['slug' => $slug])
+    </div>
+    @if($edit)
+        <livewire:shrine.edit-loader/>
+    @endif
+</main>
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\Shrine\ShrineLoader;
-use App\Http\Controllers\ShrinePageController;
 
 /*
 |--------------------------------------------------------------------------
